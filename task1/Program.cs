e21e2eb5b0940fe8ade0fe5428858c8c6bb06d88
OksanaMiheenko1412 <118578118+OksanaMiheenko1412@users.noreply.github.com>
2022-12-12 19:28:21 +0700

    / add  commit task1

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// task1 введение массива строк
+
+//Console.Clear();
+//string[] numbers = Console.ReadLine().Split(" ");
+//foreach (string i in numbers)// перебор массива без использования нумерации и
+//{
+   // Console.WriteLine(i); // вывод в терминале каждое число с новой строки
+//}
+// функция Split - противоположна void, разделяет строку
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+string[] numbers = Console.ReadLine().Split(" ");
+int row = int.Parse(numbers[0]);//количество строк
+int col = int.Parse(numbers[1]);//количество столбцов
+int[,] matrix = new int[row, col];
