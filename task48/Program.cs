2fc8c21c2facdc50ca08573ea538747cfb5cd5b3
OksanaMiheenko1412 <118578118+OksanaMiheenko1412@users.noreply.github.com>
2022-12-12 22:30:53 +0700

    / add  commit task48

---
+++ task48/Program.cs
@@ -0,0 +1,21 @@
+﻿// task48 Задайте двумерный массив размера m на n, каждый элемент в массиве
+//находится по формуле: Amn = m + n. Выведите полученный массив на экран
+// m=3, n=4. 0123;1234;2345.
+ void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++ )
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+string[] numbers = Console.ReadLine().Split(" ");
+int[,] matrix = new int[int.Parse(numbers[0]), int.Parse(numbers[1])];
+InputMatrix(matrix);
