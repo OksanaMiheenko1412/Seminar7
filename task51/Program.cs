65bd66d63f817ee237d412a380ebe32ba889248a
OksanaMiheenko1412 <118578118+OksanaMiheenko1412@users.noreply.github.com>
2022-12-13 08:18:23 +0700

    / add  commit tas51

---
+++ task51/Program.cs
@@ -0,0 +1,41 @@
+﻿// task51 Задайте двумерный массив. Найдите сумму элементов,находящихся
+// на главной диагонали (с индексами (0,0);(1,1). Пример 1472, 5923, 8424
+// Сумма элементов главной диагонали: 1+9+2=12 
+
+void InputMatrix(int[, ] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 11); // [1, 10]
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ReleaseMatrix(int[, ] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 1 && j % 2 == 1)
+            {
+                matrix[i, j] *= matrix[i, j];
+            }
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+int[] coord = Console.ReadLine().Split(" ").Select(s => int.Parse(s)).ToArray();
+int[,] matrix = new int[coord[0], coord[1]];
+Console.WriteLine("Начальная матрица: ");
+InputMatrix(matrix);
+Console.WriteLine("Результат: ");
+ReleaseMatrix(matrix);
